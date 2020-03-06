d3e64b29d0b71a913848dba8c620d0938053df2f
Maurício Dias <mau.dias@gmail.com>
2020-03-06 23:15:29 +0100

    Attempt on multiple agents spawned on demand

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -11,8 +11,18 @@ def setup():
 def step(script):
     print(f'''  - label: ":rocket: {ntpath.basename(script)}"
     command: "./{script}"
+    agents:
+      docker: true
+''')
+
+def prepare():
+    print(f'''  - label: ":rocket: Prepare agents"
+    command: "buildkite-agent start --spawn 3 --tags="docker=true""
+    agents:
+      docker: true
 ''')
 
 
 if __name__ == '__main__':
+    prepare()
     setup()
